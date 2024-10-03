45a10a0ffdea80ca80aa03a836ea7c1d64d3a88d
Swanimadhoop <swanimadhoop@gmail.com>
2024-10-03 22:26:16 +0530

    final organiser pages

--- hackathon-website/src/pages/organisers_pages/Submissions.js
+++ hackathon-website/src/pages/organisers_pages/Submissions.js
@@ -1,54 +1,51 @@
 import React from "react";
-import { MdAccountCircle } from "react-icons/md"
+import { MdAccountCircle } from "react-icons/md";
+import Button from "@mui/material/Button";
 import "./Submissions.css";
 
 export const Submissions = () => {
   return (
     <div className="winners-list">
-      <div className="div">
+      {/* <div className="div"> */}
         <div className="overlap">
           <div className="yellow-up" />
           <div className="home-and-logout">
-            <div className="overlap-group">
-              <div className="home-abt-supp">
-                <div className="navigation-pill-list">
-                  <div className="navigation-pill" />
-                  <div className="title-wrapper">
-                    <div className="title">Home</div>
-                  </div>
-                  <div className="title-wrapper">
-                    <div className="title">About</div>
-                  </div>
-                  <div className="title-wrapper">
-                    <div className="title">Support</div>
-                  </div>
-                  <div className="title-wrapper">
-                    <div className="text-wrapper">{""}</div>
-                  </div>
-                </div>
-              </div>
-              <div className="log-out">
-                <div className="logout">Log out</div>
-              </div>
+            <div className="navigation-publish">
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Support</a>
             </div>
+            <Button
+              variant="contained"
+              className="log-out-button-publish"
+              onClick={() => console.log("Logging out")}
+            >
+              Log Out
+            </Button>
+            <MdAccountCircle className="account-circle-publish" />
+            <p className="hack-a-trek-publish">
+              <span className="span">Hack’</span>
+              <span className="purple">a</span>
+              <span className="span">’Trek</span>
+            </p>
           </div>
-          <MdAccountCircle className="account-circle" />
-          <p className="hack-a-trek">
-            <span className="span">Hack’</span>
-            <span className="span">a</span>
-            <span className="span">’Trek</span>
-          </p>
+          <div className="text">
+            <div className="title-2">Hackathon Name</div>
+            <div className="title-3">Project Submissions</div>
+            <a className="link-submission" href="feedback">
+              <p className="p">1st (Name and details of the team)</p>
+            </a>
+            <a className="link-submission" href="#second-place">
+              <p className="text-wrapper-4">2nd (Name and details of the team)</p>
+            </a>
+            <a className="link-submission" href="#third-place">
+              <p className="text-wrapper-3">3rd (Name and details of the team)</p>
+            </a>
+          </div>
         </div>
-        <div className="text">
-          <div className="title-2">Hackathon Name </div>
-          <div className="title-3">Project Submissions </div>
-
-        </div>
-        <p className="p">1st (Name and details of the team)</p>
-        <p className="text-wrapper-4">2nd (Name and details of the team)</p>
-        <p className="text-wrapper-3">3rd (Name and details of the team)</p>
       </div>
-    </div>
+    // </div>
   );
 };
+
 export default Submissions;
